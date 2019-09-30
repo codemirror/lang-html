5b728f4c0f4c56641de3a823b654274f5a46a929
Marijn Haverbeke <marijn@haverbeke.nl>
2019-09-30 16:28:28 +0200

    Get the demo to run with the new build process
    It's a plain .js file for now, so that it can use moduleserve directly

---
+++ src/html.ts
@@ -0,0 +1,57 @@
+import {configureHTML} from "lezer-html"
+import {cssSyntax} from "../../lang-css"
+import {javascriptSyntax} from "../../lang-javascript"
+import {LezerSyntax, delimitedIndent, continuedIndent, indentNodeProp, openNodeProp, closeNodeProp} from "../../syntax"
+import {NodeType} from "lezer-tree"
+import {Style as s, styleNodeProp} from "../../theme"
+
+export const htmlSyntax = new LezerSyntax(configureHTML([
+  {tag: "script",
+   attrs(attrs) {
+     return !attrs.type || /^(?:text|application)\/(?:x-)?(?:java|ecma)script$|^module$|^$/i.test(attrs.type)
+   },
+   parser: javascriptSyntax.parser},
+  {tag: "style",
+   attrs(attrs) {
+     return (!attrs.lang || attrs.lang == "css") && (!attrs.type || /^(text\/)?(x-)?(stylesheet|css)$/i.test(attrs.type))
+   },
+   parser: cssSyntax.parser}
+]).withProps(
+  indentNodeProp.add(type => {
+    if (type.name == "Element") return delimitedIndent({closing: "</", align: false})
+    if (type.name == "OpenTag" || type.name == "CloseTag" || type.name == "SelfClosingTag") return continuedIndent()
+    return undefined
+  }),
+  openNodeProp.add(NodeType.match({
+    "StartTag StartCloseTag": ["EndTag", "SelfCloseEndTag"],
+    "OpenTag": ["CloseTag"]
+  })),
+  closeNodeProp.add(NodeType.match({
+    "EndTag SelfCloseEndTag": ["StartTag", "StartCloseTag"],
+    "CloseTag": ["OpenTag"]
+  })),
+  styleNodeProp.styles(NodeType.match({
+    AttributeValue: s.literal.string,
+    RawText: s.markup.content,
+    StartTag: s.bracket.angle.open,
+    StartCloseTag: s.bracket.angle.open,
+    SelfCloserEndTag: s.bracket.angle.close,
+    EndTag: s.bracket.angle.close,
+    SelfCloseEndTag: s.bracket.angle.close,
+    TagName: s.name.type,
+    MismatchedTagName: s.invalid.unexpected,
+    AttributeName: s.name.property,
+    UnquotedAttributeValue: s.name.value,
+    Is: s.operator.define,
+    EntityReference: s.literal.character,
+    CharacterReference: s.literal.character,
+    Text: s.markup.content,
+    Comment: s.comment.block,
+    ProcessingInst: s.meta.instruction,
+    DoctypeDecl: s.meta.declaration
+  }))
+))
+
+export function html() {
+  return htmlSyntax.extension
+}
